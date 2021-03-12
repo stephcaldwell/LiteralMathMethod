59efebc3f58def41b0b1e7e28af674c01847e313
stephcaldwell <77687614+stephcaldwell@users.noreply.github.com>
2021-03-12 16:34:37 -0600

    initial exercise without bonuses

---
+++ theLMMExercise.js
@@ -0,0 +1,23 @@
+let warmhugs = "Hi, I'm Olaf and I like warm hugs.";
+console.log(warmhugs);
+console.log(warmhugs.toUpperCase());
+warmhugs= warmhugs.replace ("like", "love");
+console.log(warmhugs);
+
+let beenImpaled = "Oh, look at that. I've been impaled.";
+
+console.log(beenImpaled.slice (18));
+
+let dotDotDot = "...";
+let skullBones = "I don't have a skull"+  dotDotDot + "or bones.";
+console.log(skullBones);
+
+console.log(Math.PI);
+
+let randomNumber = (Math.random());
+randomNumber *= 3;
+randomNumber = Math.floor (randomNumber);
+randomNumber++;
+
+console.log(randomNumber);
+
